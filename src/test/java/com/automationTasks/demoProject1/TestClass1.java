b2e9b69dba9b2b060ecd44b4312a0c67b36cecae
sravanthi <vgs.godha@gmail.com>
2020-02-27 21:51:03 -0800

    commit dempProject1

---
+++ src/test/java/com/automationTasks/demoProject1/TestClass1.java
@@ -0,0 +1,42 @@
+package com.automationTasks.demoProject1;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestClass1 {
+
+	public static WebDriver driver;
+
+	@BeforeMethod
+	public void lauchDriver() {
+		System.setProperty("webdriver.chrome.driver", "C:\\drivers\\chrome\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+	@Test
+	public void test1() {
+		driver.navigate().to("https://www.google.com/");
+		System.out.println("Test 1 title is : "+ driver.getTitle());
+		}
+	@Test
+	public void test2() {
+		driver.navigate().to("https://www.google.com/");
+		System.out.println("Test 2 title is : "+ driver.getTitle());
+	}
+	@Test
+	public void test3() {
+		driver.navigate().to("https://www.google.com/");
+		System.out.println("Test 3 title is : "+ driver.getTitle());
+	}
+	
+	@AfterMethod
+	public void quit() {
+		driver.quit();      
+	}
+}
